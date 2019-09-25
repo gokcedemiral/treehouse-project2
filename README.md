aac918ba4e420f3bd903af663ed689e9dae73767
gokcedemiral <egokced@gmail.com>
2019-09-25 12:23:31 +0200

    Added new description to ReadMe and added two functions show page and append page links.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # treehouse-project2
 Team Treehouse 2nd Project on Adding Pagination and listing.
+
+# Project Description
+
+A student list containing names, photos and email addresses are given. By using JS you are expected to display 10 names at a time and provide a navigation system, aka a pagination, to reach the next 10 of the list and so on. 
+
+- You are expected to adhere "Progressive enhancement principles". Meaning, once the JS is turned off, the page should continue displaying the list as a whole, and/or the basic content, functionality, message, look and feel should be accessible to all web browsers/ different display resolutions.
+
+- You are expected to use vanilla JS.
+
+- You are expected to link all JS from an external file rather than inline JS and use unobstrusive JS.
+
+- Always explain how the functions work with code comments.
+
+- Paging, pagination links and search bar needs to be written as "DRY" as possible. 
+
+# What I built
+
+
+
+
+The skills, techniques and process used to complete the project
+If you’ve attempted to earn an “Exceeds Expectations” grade, list the “Extra Credit” features you added.
--- js/script.js
+++ js/script.js
@@ -1,47 +1,87 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Erşen Gökçe Demiral
 ******************************************/
+
+/*** 
+   The global variables
+***/
+const listItems = document.querySelectorAll('li.student-item');
+const page = document.querySelector('body>div.page');
+//console.log(listItems);
+const perPage = 10;
+
+
+/*** 
+ *    Function showPage
+         displays only the 10(perPage) on the page.
+         
+      Parameters:
+         - list : all of the students' list (we will call the listItems we defined above)
+         - page : the page that the selected 10 is supposed to be displayed
+      
+      What else is there:
+         - firstIndex : The first list item's index of the page
+         - lastIndex : The last list item's index of the page
+         - for loop to iterate through the list items
+         - if condition to set whether they have the index between first and last index (including both)
+
+***/
+const showPage = (list, page) => {
+   const firstIndex = (page * perPage) - perPage;
+   const lastIndex = (page * perPage) - 1;
+   for( let i = 0; i < list.length; i++){
+      if(i>= firstIndex && i<= lastIndex){
+         list[i].style.display = '';
+      } else {
+         list[i].style.display = 'none';
+      }
+   }
+};
+
+/*** 
+ *    Function appendPageLinks
+         creates pagination buttons. 
+         places to the DOM. 
+         adds functionality to the buttons. 
+         
+      Parameters:
+         - list : all of the students' list (we will call the listItems we defined above)
+         - page : the page that the selected 10 is supposed to be displayed
+      
+      What else is there:
+         - firstIndex : The first list item's index of the page
+         - lastIndex : The last list item's index of the page
+         - for loop to iterate through the list items
+         - if condition to set whether they have the index between first and last index (including both)
+
+***/
+const appendPageLinks = (list) => {
+   const pageAmount = Math.ceil( list.length / perPage);
+   const div = document.createElement('div');
+   div.className = 'pagination';
+   page.appendChild(div);
+   const paginationUl = document.createElement('ul');
+   div.appendChild(paginationUl);
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+   for( let i = 0; i < pageAmount; i++){
+      const pageNumber = i+1;
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.textContent = pageNumber;
+      li.appendChild(a);
+      paginationUl.appendChild(li);
+      a.addEventListener('click', () => {
+         showPage(listItems, pageNumber);
+         a.className = 'active';
+      });
+   }
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
+};
+appendPageLinks(listItems);
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
 
 
